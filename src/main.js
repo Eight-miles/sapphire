6dcaac6de63adfc3130d3e1f4f81c1cc059c1d97
yanz <xiaoming@163.com>
2018-09-14 18:32:20 +0800

    电影项目初始化

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+//按需导入mint-ui
+import { Header,Tabbar, TabItem  } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+
+//导入mui的css 样式
+import './lib/dist/css/mui.css'
+
+
+//导入app模块
+import app from './App.vue'
+
+
+var vm = new Vue({
+	el:'#app',
+	data:{},
+	render:c => c(app)
+})
